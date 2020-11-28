3f748d3d9b12559df80085e3240b190728f2a5f8
jiang-hr <jianghanrui@outlook.com>
2020-11-28 10:05:29 +0800

    change cal

--- qq_bot_ai/plugins/cal.py
+++ qq_bot_ai/plugins/cal.py
@@ -1,4 +1,5 @@
 from nonebot import on_command
+from nonebot.plugin import on_regex
 from nonebot.rule import to_me
 from nonebot.adapters.cqhttp import Bot, Event
 from sympy import *
@@ -8,8 +9,7 @@ from base64 import *
 from libnum import *
 from random import *
 
-calculator = on_command("cal", rule=None, priority=1)
-
+calculator = on_regex(".*", rule=None, priority=1000)
 
 
 @calculator.handle()
@@ -19,15 +19,22 @@ async def handle_first_receive(bot: Bot, event: Event, state: dict):
     if args:
         state["string"] = args  # 如果用户发送了参数则直接赋值
     temp_state_str = state["string"]
-    err = "\"请勿尝试使用可能破坏蓝的危险方法\""
     des = "\"请勿破坏蓝的算力\""
     forbid = "\"这句话不被允许执行\""
     priva = "\"不允许访问这里\""
     a = False
+    stop = False
+    try:
+        temp = float(state["string"])
+        stop = True
+    except ValueError:
+        pass
     if re.match("\\s*\".*\"\\s*", state["string"]) and state["string"].count("\"") == 2:
         a = True
     if re.match("\\s*\'.*\'\\s*", state["string"]) and state["string"].count("\'") == 2:
         a = True
+    if len(state["string"]) > 200:
+        stop = True
     cnt = state["string"].count("*") + 2 * state["string"].count("integrate") +\
         state["string"].count("sin")+state["string"].count("cos") +\
         3 * state["string"].count("limit") + 2*state["string"].count("gamma") +\
@@ -35,46 +42,38 @@ async def handle_first_receive(bot: Bot, event: Event, state: dict):
         state["string"].count("**") + 2 * state["string"].count("exp")
     if cnt > 12:
         state["string"] = forbid
-    if len(state["string"]) > 200:
-        state["string"] = "\"请重新输入\""
     if re.match(".*__.*__", state["string"]):
-        state["string"] = "\"不允许使用魔法方法\""
+        stop = True
     if re.match(".*os\\s*\\.", state["string"]):
-        state["string"] = err
+        stop = True
     if re.match(".*(to_me|Bot|Event|on_command|nonebot)", state["string"]):
-        state["string"] = priva
+        stop = True
     if re.match(".*sleep", state["string"]):
+        stop = True
+    if re.match(".*(integrate|exp|gamma)\\s*\\(\\s*\\d{4,}", state["string"]):
         state["string"] = forbid
-    if re.match(".*gamma\\s*\\(\\s*\\d{4,}", state["string"]):
-        state["string"] = forbid
-    if re.match(".*gamma\\s*\\(.*\\s*\\d{1,}\\s*\\*+\\s*\\d{1,}", state["string"]):
+    if re.match(".*(integrate|exp|gamma)\\s*\\(.*\\s*\\d{1,}\\s*\\*+\\s*\\d{1,}", state["string"]):
         state["string"] = forbid
     if re.match(".*dir\\s*\\(\\s*\\)", state["string"]):
         state["string"] = priva
     if re.match(".*\\*\\*\\s*gamma", state["string"]):
         state["string"] = forbid
-    if re.match(".*integrate.*\\*\\*\\s*\\d{2,}", state["string"]):
+    if re.match(".*(integrate|exp|gamma).*\\*\\*\\s*\\d{2,}", state["string"]):
         state["string"] = forbid
-    if re.match(".*(help|main|read|open|import|exec)", state["string"]):
-        state["string"] = err
-    if re.match(".*eval(\\s*\\()", state["string"]):
-        state["string"] = err
-    if re.match(".*for\\s", state["string"]):
-        state["string"] = des
-    if re.match(".*while", state["string"]):
-        state["string"] = des
-    if state["string"].find("range") >= 0:
-        state["string"] = des
+    if re.match(".*(range|help|main|read|open|import|exec|for\\s|while|eval\\s*\\()", state["string"]):
+        stop = True
     if re.match(".*\\*\\*\\s*\\d+\\s*\\*\\*\\s*\\d+", state["string"]):
         state["string"] = forbid
-    if re.match(".*\\*\\*\\s\\d{4,}", state["string"]):
+    if re.match(".*\\*\\*\\s*\\d{4,}", state["string"]):
         state["string"] = des
     if re.match(".*\\s*\\d{2,}\\s*\\*\\*\\s*\\d{3,}", state["string"]):
         state["string"] = des
     if state["string"].find("\n") >= 0:
-        state["string"] = "\"请勿尝试换行\""
+        stop = True
     if a == True:
         state["string"] = temp_state_str
+    if stop:
+        state["string"] = ""
 
 
 def cal(state: dict):
@@ -89,15 +88,14 @@ def cal(state: dict):
     umi = "rbq"
     rbq = "rbq"
     真红 = "真红"
-    透 = "透"
-    喜欢 = "我永远喜欢"
     ntr = "爬"
-    伊卡 = "rbq"
     string = state["string"]
+    if string == "":
+        return ""
     try:
         string_calculator = str(eval(string))
     except BaseException as e:
-        string_calculator = "蓝执行出现错误，错误原因如下：\n" + str(e.args)
+        string_calculator = ""
     if len(string_calculator) > 500:
         string_calculator = string_calculator[:500] + '...更多省略'
     return string_calculator
@@ -106,4 +104,6 @@ def cal(state: dict):
 @calculator.got("string", prompt=" ")
 async def handle_string(bot: Bot, event: Event, state: dict):
     string_calculator = cal(state)
+    if string_calculator == "":
+        await calculator.finish()
     await calculator.finish(string_calculator)
